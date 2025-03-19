5d77e3e88290e8d53ba06af21e230903fabd5c3d
Tuns <nguyennttuan8604@gmail.com>
2025-03-19 14:24:29 +0700

    Angular basic

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { Pagination } from './shared/models/pagination';
+import { Product } from './shared/models/products';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Sibidi';
+export class AppComponent implements OnInit{
+  baseUrl = 'https://localhost:5001/api/'
+  private http = inject(HttpClient)
+  title = 'Skishop';
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: (response: any) => { // Tạm sử dụng kiểu `any` nếu cần
+        this.products = response as Product[]; // Gán và ép kiểu response
+      },
+      error: error => console.log(error),
+      complete: () => console.log('complete')
+    })
+  }
+
 }
